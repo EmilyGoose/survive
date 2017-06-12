52f519e55d39d94ec1d393299ee5a292ddeb14ec
Misha Larionov <MishaLarionov@users.noreply.github.com>
2017-06-12 12:42:24 -0400

    Fixed distance calculation (whoops)

--- src/Game.java
+++ src/Game.java
@@ -132,9 +132,9 @@ public class Game {
                     ((ResourceGenerator) Game.world.getItemAtIndex(i)).update();
                 } else if (object instanceof CampFire) {
                     //Find the distance between the fire and the player
-                    int xDiff = Game.player.yPos - object.yPos;
-                    int yDiff = Game.player.xPos - object.yPos;
-                    double distance = Math.sqrt(xDiff * xDiff + yDiff + yDiff);
+                    int xDiff = Game.player.xPos - object.xPos;
+                    int yDiff = Game.player.yPos - object.yPos;
+                    double distance = Math.sqrt(xDiff * xDiff + yDiff * yDiff);
                     //Warm the player if they are near enough to the campfire
                     if (distance <= ((CampFire) object).getRange()) {
                         Game.player.addWarmth(10);
