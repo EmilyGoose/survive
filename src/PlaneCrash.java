f1c9753bc34df8f8dc496b654ebd8c05b7cd9d69
Misha Larionov <MishaLarionov@users.noreply.github.com>
2017-06-14 13:20:33 -0400

    fix: The sequel

--- src/PlaneCrash.java
+++ src/PlaneCrash.java
@@ -5,6 +5,8 @@
 
 public class PlaneCrash extends CampFire{
 
+    private double size = 1;
+
     PlaneCrash() {
         super(2500, 2500);
         this.setImageName("crashedplane");
@@ -18,5 +20,11 @@ public class PlaneCrash extends CampFire{
     public boolean addFuel(GameObject fuel) {
         //The plane fire burns until it burns out and then no more
         return false;
+    }
+
+    public void update() {
+        //Update the fire's size every frame
+        this.size -= 1.0/(30 * 60); //Fire depletes from full in 30 seconds at 60 FPS
+        //The plane does not delete itself
     }
 }
